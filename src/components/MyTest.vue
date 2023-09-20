e1d1b610318a10e174c0466ac631dd404dada6ef
Maksim Belogurov <maxbelogurov@gmail.com>
2023-09-20 15:02:22 +0200

    init: start project

---
+++ src/components/MyTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="title">Home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyTest"
+}
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+</style>
